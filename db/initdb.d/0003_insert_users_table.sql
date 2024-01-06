db166a447decc267538cce01f24efd0d99148924
kyoF <fujiki.kyosuke@gmail.com>
2024-01-06 19:13:44 +0900

    add: init sql

---
+++ db/initdb.d/0003_insert_users_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, name, mail, pass)
+VALUES
+  ('fujiki', 'fujiki', 'fujiki@twitter.com', 'fujikipass'),
+  ('akazawa', 'akazawa', 'akazawa@twitter.com', 'akazawapass'),
+  ('mori', 'mori', 'mori@twitter.com', 'moripass');
+
