887cec13869061bd2ab287f73bf4d7a200be5113
kyosukeFujiki <fujiki.kyosuke.tl@mynavi.jp>
2024-01-12 18:23:05 +0900

    add: link for tweet/

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <h2>HomeHome</h2>
-      <h3>HomeHomeHome</h3>
-      <h4>HomeHomeHomeHome</h4>
-      <h5>HomeHomeHomeHomeHome</h5>
+      <Link href="/home">
+        /tweets
+      </Link>
     </>
   );
 };
