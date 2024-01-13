9e260b7253bb5d434fb58f63f49fd5d30a7f7a4c
kyoF <fujiki.kyosuke@gmail.com>
2024-01-13 18:44:51 +0900

    add: check link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <>
       <header>
         <div>==========</div>
         <div>Header!!!</div>
+        <div><Link href={'/home'}>home</Link></div>
+        <div><Link href="/tweet/create">create</Link></div>
+        <div><Link href={'/test'}>test</Link></div>
         <div>==========</div>
       </header>
     </>
