c5395ca4f089898155a01e405dd52ee3c827bf4c
kyoF <fujiki.kyosuke@gmail.com>
2024-03-05 20:36:52 +0900

    add: login repository

--- backend/domain/repository/user.go
+++ backend/domain/repository/user.go
@@ -5,4 +5,5 @@ import "backend/domain/entity"
 type IUserRepository interface {
 	FetchUser(userId string) (*entity.User, error)
 	SignUp(userId string, name string, email string, password string) error
+    Login(email string, password string) error
 }
