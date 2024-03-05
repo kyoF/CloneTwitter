97b7b70008d3c74f07c2855676dfe8dc73f4bee0
kyoF <fujiki.kyosuke@gmail.com>
2024-03-05 19:47:34 +0900

    add: signup router

--- backend/presentation/router/router.go
+++ backend/presentation/router/router.go
@@ -3,11 +3,15 @@ package router
 import "github.com/labstack/echo/v4"
 
 func InitRoute(
+    signUpRouter ISignUpRouter,
+    loginRouter ILoginRouter,
     fetchUserRouter IFetchUserRouter,
     fetchAllTweetRouter IFetchAllTweetsRouter,
     createTweetRouter ICreateTweetRouter,
 ) {
     e := echo.New()
+    e.GET("/signup", signUpRouter.Route())
+    e.GET("/login", loginRouter.Route())
     e.GET("/user/:id", fetchUserRouter.Route())
     e.GET("/tweets", fetchAllTweetRouter.Route())
     e.POST("/user/:id/tweet/create", createTweetRouter.Route())
