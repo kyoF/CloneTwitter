b982bf4f57304b8a9fa4eb54e926bf920c5333d4
kyoF <fujiki.kyosuke@gmail.com>
2024-03-05 19:47:58 +0900

    add: signup infra

--- backend/infrastructure/user.go
+++ backend/infrastructure/user.go
@@ -7,20 +7,30 @@ import (
 	"backend/domain/repository"
 )
 
-type userInfra struct{
-    db *gorm.DB
+type userInfra struct {
+	db *gorm.DB
 }
 
 func NewUserRepository(db *gorm.DB) repository.IUserRepository {
-    return &userInfra{db}
+	return &userInfra{db}
 }
 
 func (i *userInfra) FetchUser(userId string) (*entity.User, error) {
-    var user *entity.User
-    err := i.db.Where("user_id = ?", userId).First(&user).Error
-    if err != nil {
-        return nil, err
-    }
-    return user, nil
+	var user *entity.User
+	err := i.db.Where("user_id = ?", userId).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
+func (i *userInfra) SignUp(userId string, name string, email string, password string) error {
+	user, err := entity.NewUser(userId, name, email, password)
+	if err != nil {
+		return err
+	}
+	f := func(tx *gorm.DB) error {
+        return tx.Create(&user).Error
+	}
+    return i.db.Transaction(f)
+}
