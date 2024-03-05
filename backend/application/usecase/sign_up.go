90aa8955d46f81fde7e3e53016b343bc47188d9e
kyoF <fujiki.kyosuke@gmail.com>
2024-03-05 19:48:11 +0900

    add: signup usecase

---
+++ backend/application/usecase/sign_up.go
@@ -0,0 +1,21 @@
+package usecase
+
+import "backend/domain/repository"
+
+type ISignUpUsecase interface {
+    SignUp(userId string, name string, email string, password string) error
+}
+
+type signUpUsecase struct {
+    userRepo repository.IUserRepository
+}
+
+func NewSignUpUsecase(userRepo repository.IUserRepository) ISignUpUsecase {
+    return &signUpUsecase{userRepo}
+}
+
+func (uc *signUpUsecase) SignUp(userId string, name string, email string, password string) error {
+    err := uc.userRepo.SignUp(userId, name, email, password)
+    return err
+}
+
