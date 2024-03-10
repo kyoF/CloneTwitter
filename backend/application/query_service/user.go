10f940c2eae19c8c683992ff8f22b7613e39dcae
kyoF <fujiki.kyosuke@gmail.com>
2024-03-10 18:28:43 +0900

    add: user query service

---
+++ backend/application/query_service/user.go
@@ -0,0 +1,39 @@
+package query_service
+
+import (
+	"backend/application/query_service/dto"
+	"backend/domain/repository"
+)
+
+type IUserQueryService interface {
+    Fetch(userId string) (*dto.FetchUserDto, error)
+}
+
+type userQueryService struct{
+    userRepo repository.IUserRepository
+    tweetRepo repository.ITweetRepository
+}
+
+func NewUserQueryService(userRepo repository.IUserRepository, tweetRepo repository.ITweetRepository) IUserQueryService {
+    return &userQueryService{
+        userRepo,
+        tweetRepo,
+    }
+}
+
+func (qs *userQueryService) Fetch(userId string) (*dto.FetchUserDto, error) {
+    user, err := qs.userRepo.Fetch(userId)
+    if err != nil {
+        return nil, err
+    }
+    tweets, err := qs.tweetRepo.FetchAll()
+    if err != nil {
+        return nil, err
+    }
+    dto := dto.NewFetchUserDto(
+        userId,
+        user.Name,
+        tweets,
+    )
+    return dto, nil
+}
