a82123d896bbd4142d820256b32c3685d34a0699
kyoF <fujiki.kyosuke@gmail.com>
2024-03-10 18:30:43 +0900

    fix: init route

--- backend/presentation/router/router.go
+++ backend/presentation/router/router.go
@@ -1,21 +1,30 @@
 package router
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func InitRoute(
-    signUpRouter ISignUpRouter,
-    loginRouter ILoginRouter,
-    fetchUserRouter IFetchUserRouter,
-    fetchAllTweetRouter IFetchAllTweetsRouter,
-    createTweetRouter ICreateTweetRouter,
+	authRouter IAuthRouter,
+	userRouter IUserRouter,
+	tweetRouter ITweetRouter,
 ) {
-    e := echo.New()
-    e.GET("/signup", signUpRouter.Route())
-    e.GET("/login", loginRouter.Route())
-    e.GET("/user/:id", fetchUserRouter.Route())
-    e.GET("/tweets", fetchAllTweetRouter.Route())
-    e.POST("/user/:id/tweet/create", createTweetRouter.Route())
+	e := echo.New()
 
-    e.Logger.Fatal(e.Start(":8888"))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+    a := e.Group("/api")
+
+    a.POST("/signup", authRouter.SignUp())
+	a.POST("/login", authRouter.Login())
+
+    a.GET("/user/:userId", userRouter.Fetch())
+
+    a.GET("/tweets", tweetRouter.FetchAll())
+    a.GET("/tweet/:tweetId", tweetRouter.Fetch())
+    a.POST("/tweet/create", tweetRouter.Create())
+
+	e.Logger.Fatal(e.Start(":8888"))
 }
-
