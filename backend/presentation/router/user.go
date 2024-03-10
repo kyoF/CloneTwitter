9e0355be8982072c4d74facb3b149b2ac170a100
kyoF <fujiki.kyosuke@gmail.com>
2024-03-10 18:30:32 +0900

    add: user router

---
+++ backend/presentation/router/user.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"backend/application/query_service"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type IUserRouter interface {
+	Fetch() echo.HandlerFunc
+}
+
+type userRouter struct {
+	qs query_service.IUserQueryService
+}
+
+func NewUserRouter(qs query_service.IUserQueryService) IUserRouter {
+	return &userRouter{qs}
+}
+
+func (u *userRouter) Fetch() echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		userId := ctx.Param("id")
+		dto, err := u.qs.Fetch(userId)
+		if err != nil {
+			ctx.Error(err)
+		}
+		return ctx.JSON(http.StatusOK, dto)
+	}
+}
