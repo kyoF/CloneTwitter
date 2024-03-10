1b90192581cb519b9f5caab0d04d2d8931aabebf
kyoF <fujiki.kyosuke@gmail.com>
2024-03-10 18:31:12 +0900

    fix: really use di

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"backend/application/query_service"
-	"backend/application/usecase"
-	"backend/infrastructure"
+	"backend/di"
 	"backend/infrastructure/config"
 	"backend/presentation/router"
 )
@@ -11,29 +9,9 @@ import (
 func main() {
 	db := config.NewDB()
 
-	tweetRepo := infrastructure.NewTweetRepository(db)
-	userRepo := infrastructure.NewUserRepository(db)
-
-	fetchUserQueryService := query_service.NewFetchUserQueryService(userRepo, tweetRepo)
-	fetchUserRouter := router.NewFetchUserRouter(fetchUserQueryService)
-
-	fetchAllTweetsQueryService := query_service.NewFetchAllTweetsQueryService(tweetRepo)
-	fetchAllTweetsRouter := router.NewFetchAllTweetsRouter(fetchAllTweetsQueryService)
-
-	createTweetUsecase := usecase.NewCreateTweetUsecase(tweetRepo)
-	createTweetRouter := router.NewCreateTweetRouter(createTweetUsecase)
-
-	signUpUsecase := usecase.NewSignUpUsecase(userRepo)
-	signUpRouter := router.NewSignUpRouter(signUpUsecase)
-
-	loginQueryService := query_service.NewLoginQueryService(userRepo)
-	loginRouter := router.NewLoginRouter(loginQueryService)
-
 	router.InitRoute(
-		fetchUserRouter,
-		fetchAllTweetsRouter,
-		createTweetRouter,
-		loginRouter,
-		signUpRouter,
+        di.Auth(db),
+        di.User(db),
+        di.Tweet(db),
 	)
 }
