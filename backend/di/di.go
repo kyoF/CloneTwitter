21cec7848e328ea494f4585dc0f4a1f79ab6c366
kyoF <fujiki.kyosuke@gmail.com>
2024-03-10 18:28:52 +0900

    add: di package

---
+++ backend/di/di.go
@@ -0,0 +1,29 @@
+package di
+
+import (
+	"backend/application/query_service"
+	"backend/application/usecase"
+	"backend/infrastructure"
+	"backend/presentation/router"
+
+	"gorm.io/gorm"
+)
+
+func Auth(db *gorm.DB) router.IAuthRouter {
+    authRepo := infrastructure.NewAuthInfra(db)
+    authUsecase := usecase.NewAuthUsecase(authRepo)
+    return router.NewAuthRouter(authUsecase)
+}
+
+func User(db *gorm.DB) router.IUserRouter {
+    userRepo := infrastructure.NewUserRepository(db)
+    userQueryService := query_service.NewUserQueryService(userRepo)
+    return router.NewUserRouter(userQueryService)
+}
+
+func Tweet(db *gorm.DB) router.ITweetRouter {
+	tweetRepo := infrastructure.NewTweetRepository(db)
+	queryService := query_service.NewTweetUsecase(tweetRepo)
+	usecase := usecase.NewTweetUsecase(tweetRepo)
+	return router.NewTweetRouter(queryService, usecase)
+}
