21a9abe644293ff579fdf6bd5993d4e59a556e4a
kyoF <fujiki.kyosuke@gmail.com>
2024-03-12 21:04:31 +0900

    add: react icons

--- frontend/src/components/tweet/Card.tsx
+++ frontend/src/components/tweet/Card.tsx
@@ -2,11 +2,11 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
-import { HeartIcon, PersonIcon } from '@radix-ui/react-icons';
+import { CiUser, CiHeart } from "react-icons/ci";
 
 import type { TTweet } from '@/types';
 
-export const TweetCard = ({...tweet}: TTweet) => {
+export const TweetCard = ({ ...tweet }: TTweet) => {
   const { userId, text, likesCount } = tweet;
 
   const [like, setLike] = useState<number>(likesCount);
@@ -14,23 +14,23 @@ export const TweetCard = ({...tweet}: TTweet) => {
 
   const toggleLike = () => {
     if (isLike) {
-      setLike(like-1);
+      setLike(like - 1);
     } else {
-      setLike(like+1);
+      setLike(like + 1);
     }
     setIsLike(!isLike)
   }
 
-  return(
+  return (
     <>
-      <PersonIcon/>
+      <CiUser />
       <Link href={`/user/${userId}`}>
         @{userId}
       </Link>
       <div>{text}</div>
       <button onClick={toggleLike}>
         <div>
-          <HeartIcon />
+          <CiHeart />
         </div>
       </button>
       {like}
