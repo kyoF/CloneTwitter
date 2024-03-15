2de50996afe2ae4c86fedeb8db64e72375086cdb
kyoF <fujiki.kyosuke@gmail.com>
2024-03-15 17:53:13 +0900

    del: constructor

--- backend/domain/entity/user.go
+++ backend/domain/entity/user.go
@@ -5,12 +5,3 @@ type User struct {
 	Name    string `json:"name" gorm:"column:name"`
 	Profile string `json:"profile" gorm:"column:profile"`
 }
-
-func NewUser(userId string, name string, profile string) (*User, error) {
-	user := &User{
-		UserId:  userId,
-		Name:    name,
-		Profile: profile,
-	}
-	return user, nil
-}
