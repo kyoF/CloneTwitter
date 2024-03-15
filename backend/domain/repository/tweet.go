a6144a74bc967aacab01b234a9f7239fcc929822
kyoF <fujiki.kyosuke@gmail.com>
2024-03-15 17:53:24 +0900

    chore: formatter

--- backend/domain/repository/tweet.go
+++ backend/domain/repository/tweet.go
@@ -3,8 +3,7 @@ package repository
 import "backend/domain/entity"
 
 type ITweetRepository interface {
-    FetchAll() ([]entity.Tweet, error)
-    Fetch(tweetId string) (entity.Tweet, error)
-    Create(userId string, text string) (*entity.Tweet, error)
+	FetchAll() ([]entity.Tweet, error)
+	Fetch(tweetId string) (entity.Tweet, error)
+	Create(userId string, text string) (*entity.Tweet, error)
 }
-
