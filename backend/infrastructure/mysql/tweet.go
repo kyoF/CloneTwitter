f856bfc3b65b5cd2a7c3632ea1ccb843613822d2
kyoF <fujiki.kyosuke@gmail.com>
2024-03-15 17:53:56 +0900

    chore: formatter

--- backend/infrastructure/mysql/tweet.go
+++ backend/infrastructure/mysql/tweet.go
@@ -1,14 +1,15 @@
 package mysql
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 
 	"backend/domain/entity"
 	"backend/domain/repository"
 )
 
-type tweetInfra struct{
-    db *gorm.DB
+type tweetInfra struct {
+	db *gorm.DB
 }
 
 func NewTweetRepository(db *gorm.DB) repository.ITweetRepository {
@@ -16,27 +17,29 @@ func NewTweetRepository(db *gorm.DB) repository.ITweetRepository {
 }
 
 func (i *tweetInfra) FetchAll() ([]entity.Tweet, error) {
-    var tweets []entity.Tweet
-    err := i.db.Find(&tweets).Error
-    return tweets, err
+	var tweets []entity.Tweet
+	err := i.db.Find(&tweets).Error
+	return tweets, err
 }
 
 func (i *tweetInfra) Fetch(tweetId string) (entity.Tweet, error) {
-    var tweets entity.Tweet
-    err := i.db.Where("tweet_id = ?", tweetId).Find(&tweets).Error
-    return tweets, err
+	var tweets entity.Tweet
+	err := i.db.Where("tweet_id = ?", tweetId).Find(&tweets).Error
+	return tweets, err
 }
 
 func (i *tweetInfra) Create(userId string, text string) (*entity.Tweet, error) {
-    var tweet *entity.Tweet
-    err := i.db.Transaction(func(tx *gorm.DB) error {
-        tweet, err := entity.NewTweet(userId, text)
-        if err != nil {
-            return err
-        }
-        err = i.db.Create(&tweet).Error
-        return err
-    })
-    return tweet, err
+	var tweet *entity.Tweet
+	tweetId := uuid.NewString()
+	err := i.db.Transaction(func(tx *gorm.DB) error {
+		tweet = &entity.Tweet{
+			TweetId:    tweetId,
+			UserId:     userId,
+			Text:       text,
+			LikesCount: 0,
+		}
+		err := i.db.Create(&tweet).Error
+		return err
+	})
+	return tweet, err
 }
-
