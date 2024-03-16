52734aae3b1566256523c1f6a975afeaf7394d78
kyoF <fujiki.kyosuke@gmail.com>
2024-03-16 19:52:42 +0900

    add: doujou

--- backend/domain/repository/tweet.go
+++ backend/domain/repository/tweet.go
@@ -4,6 +4,7 @@ import "backend/domain/entity"
 
 type ITweetRepository interface {
 	FetchAll() ([]entity.Tweet, error)
+	FetchAllByUserId(userId string) ([]entity.Tweet, error)
 	Fetch(tweetId string) (entity.Tweet, error)
 	Create(userId string, text string) (*entity.Tweet, error)
 }
