66f2e588b4f33d49dabb26d48d8aebe72d8d860c
kyoF <fujiki.kyosuke@gmail.com>
2024-03-17 19:00:57 +0900

    fix: delete tweet button and user container and add user body

--- frontend/src/app/user/[userId]/page.tsx
+++ frontend/src/app/user/[userId]/page.tsx
@@ -1,9 +1,23 @@
-import { UserContainer } from "@/components/user/Container";
+import type { TTweet, TUser } from '@/types';
+import { UserHeader } from '@/components/user/Header';
+import { UserBody } from '@/components/user/Body';
 
 export default async function UserPage({ params }: { params: { userId: string } }) {
+  const res: Response = await fetch(
+    `${process.env.API_URL}/user/${params.userId}/tweets`,
+    { cache: 'no-store' }
+  );
+  const tweets: TTweet[] = await res.json();
+
+  const res2: Response = await fetch(
+    `${process.env.API_URL}/user/${params.userId}`,
+    { cache: 'no-store' }
+  );
+  const user: TUser = await res2.json();
   return (
     <>
-      <UserContainer userId={params.userId} />
+      <UserHeader user={user} />
+      <UserBody tweets={tweets} />
     </>
   );
 }
