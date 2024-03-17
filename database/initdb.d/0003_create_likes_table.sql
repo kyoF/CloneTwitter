3683b55d53b2d55e84e08a28c211a0ac827bab11
kyoF <fujiki.kyosuke@gmail.com>
2024-03-17 10:54:45 +0900

    fix: sql

--- database/initdb.d/0003_create_likes_table.sql
+++ database/initdb.d/0003_create_likes_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE likes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL UNIQUE,
-    tweet_id CHAR(36) NOT NULL UNIQUE,
+    user_id VARCHAR(255) NOT NULL,
+    tweet_id CHAR(36) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (tweet_id, user_id),
+    UNIQUE (user_id, tweet_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (tweet_id) REFERENCES tweets(tweet_id)
 );
--- database/initdb.d/0006_insert_likes_table.sql
+++ database/initdb.d/0006_insert_likes_table.sql
@@ -2,7 +2,7 @@ INSERT INTO likes (user_id, tweet_id)
 VALUES
   ('fujiki', 'akazawa1'),
   ('mori', 'akazawa1'),
-  ('fujiki', 'mori1');
+  ('fujiki', 'mori1'),
   ('akazawa', 'mori1'),
   ('mori', 'fujiki1');
 
