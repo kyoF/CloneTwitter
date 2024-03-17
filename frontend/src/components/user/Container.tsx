a7e70430a45bf25b01b1b1abc001700b2deb6209
kyoF <fujiki.kyosuke@gmail.com>
2024-03-17 17:42:23 +0900

    del: console.log for debug

--- frontend/src/components/user/Container.tsx
+++ frontend/src/components/user/Container.tsx
@@ -8,16 +8,12 @@ export const UserContainer = async ({ userId }: { userId: string }) => {
     { cache: 'no-store' }
   );
   const tweets: TTweet[] = await res.json();
-  console.log(tweets)
-  console.log(`${process.env.API_URL}/user/${userId}/tweets`);
-  console.log(res);
 
   const res2: Response = await fetch(
     `${process.env.API_URL}/user/${userId}`,
     { cache: 'no-store' }
   );
   const user: TUser = await res2.json();
-  console.log(user)
 
   return (
   <>
