80ce4ab1e2582a0c5f37a09538337cd9a1274b27
kyoF <fujiki.kyosuke@gmail.com>
2024-05-30 23:33:01 +0900

    fix: auth process ( in progress )

--- backend/presentation/router/auth.go
+++ backend/presentation/router/auth.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"backend/application/usecase"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -33,15 +34,28 @@ func (r *authRouter) SignUp() echo.HandlerFunc {
 	}
 }
 
+type loginReq struct {
+	userId   string `json:"userId"`
+	email    string `json:"email"`
+	password string `json:"password"`
+}
+
 func (r *authRouter) Login() echo.HandlerFunc {
 	return func(ctx echo.Context) error {
-		userId := ctx.Param("userId")
-		email := ctx.Param("email")
-		password := ctx.Param("password")
-		err := r.uc.Login(email, userId, password)
-		if err != nil {
+		req := new(loginReq)
+		if err := ctx.Bind(&req); err != nil {
 			ctx.Error(err)
 		}
+		fmt.Println(req.email)
+		fmt.Println(req.password)
+		fmt.Println(req.userId)
+		// userId := ctx.Param("userId")
+		// email := ctx.Param("email")
+		// password := ctx.Param("password")
+		// err := r.uc.Login(email, userId, password)
+		// if err != nil {
+		// 	ctx.Error(err)
+		// }
 		return ctx.JSON(http.StatusOK, nil)
 	}
 }
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,8 +1,20 @@
-export default function Login() {
+export default async function Login() {
+  const body = {
+    userId: "test",
+    email: "test@test.com",
+    password: "test-password",
+  }
+  console.log(body);
   const res: Response = await fetch(
     `${process.env.API_URL}/login`,
-    { cache: 'no-store' }
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json", },
+      body: JSON.stringify(body),
+      cache: 'no-store'
+    }
   );
+  console.log(res);
   return <div>login</div>;
 }
 
