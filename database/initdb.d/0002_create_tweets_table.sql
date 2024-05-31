c310c09be05cfc4c9ee1ca8678f3e851e86557d1
kyoF <fujiki.kyosuke@gmail.com>
2024-05-31 12:59:28 +0900

    add: unique at tweet_id

--- database/initdb.d/0002_create_tweets_table.sql
+++ database/initdb.d/0002_create_tweets_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tweets (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    tweet_id CHAR(36) NOT NULL,
+    tweet_id CHAR(36) NOT NULL UNIQUE,
     user_id VARCHAR(255) NOT NULL,
     text TEXT NOT NULL,
     likes_count INT DEFAULT 0,
