2776e6e9c0880b19c49d682da95224836d896724
kyoF <fujiki.kyosuke@gmail.com>
2024-06-06 22:58:14 +0900

    fix: router layer for auth

--- backend/presentation/router/auth.go
+++ backend/presentation/router/auth.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"backend/application/usecase"
+	"backend/utils"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -21,11 +22,21 @@ func NewAuthRouter(uc usecase.IAuthUsecase) IAuthRouter {
 }
 
 func (r *authRouter) SignUp() echo.HandlerFunc {
+	type req struct {
+		UserId   string `json:"userId"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 	return func(ctx echo.Context) error {
-		userId := ctx.Param("userId")
-		email := ctx.Param("email")
-		password := ctx.Param("password")
-		err := r.uc.SignUp(userId, email, password)
+		var req req
+		if err := ctx.Bind(&req); err != nil {
+			ctx.Error(err)
+		}
+		hashedPassword, err := utils.HashPassword(req.Password)
+		if err != nil {
+			ctx.Error(err)
+		}
+		err = r.uc.SignUp(req.UserId, req.Email, hashedPassword)
 		if err != nil {
 			ctx.Error(err)
 		}
@@ -44,7 +55,11 @@ func (r *authRouter) Login() echo.HandlerFunc {
 		if err := ctx.Bind(&req); err != nil {
 			ctx.Error(err)
 		}
-		err := r.uc.Login(req.Email, req.UserId, req.Password)
+		hashedPassword, err := utils.HashPassword(req.Password)
+		if err != nil {
+			ctx.Error(err)
+		}
+		err = r.uc.Login(req.Email, req.UserId, hashedPassword)
 		if err != nil {
 			ctx.Error(err)
 		}
